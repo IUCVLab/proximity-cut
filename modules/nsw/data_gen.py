3db0727a8b0f85c99b15679012e0bba267ce8147
Станислав Протасов <s.protasov@innopolis.ru>
2020-07-03 15:49:13 +0300

    additional tests

--- modules/nsw/data_gen.py
+++ modules/nsw/data_gen.py
@@ -5,7 +5,7 @@ from numpy.linalg import norm as vector_norm
 def gen(N=400, border=0.8):
     values = []
     for i in range(N):
-        p = np.array([random.random(), random.random()])
+        p = np.array([1 - 2 * random.random(), 1 - 2 * random.random()])
         cls = 1 if vector_norm(p) > border else 0
         values.append((p, cls))
     return values
@@ -13,7 +13,7 @@ def gen(N=400, border=0.8):
 def gen2(N=400, border1=0.3, border2=0.9):
     values = []
     for i in range(N):
-        p = np.array([random.random(), random.random()])
+        p = np.array([1 - 2 * random.random(), 1 - 2 * random.random()])
         cls = 1 if border2 > vector_norm(p) > border1 else 0
         values.append((p, cls))
     return values
@@ -21,7 +21,7 @@ def gen2(N=400, border1=0.3, border2=0.9):
 def gen_kd(N=400, k=3, border=0.3):
     values = []
     for i in range(N):
-        p = np.array([random.random() for _ in range(k)])
+        p = np.array([1 - 2 * random.random() for _ in range(k)])
         cls = 1 if vector_norm(p) > border else 0
         values.append((p, cls))
     return values
